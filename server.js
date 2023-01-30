45d8a252b0c2b2c95eb22ab9f8638420b12c0c99
Liujie Zheng <liujiez@email.unc.edu>
2023-01-29 21:37:49 -0500

    server new

--- server.js
+++ server.js
@@ -18,17 +18,14 @@ const port = argv.port == undefined ? 3000 : argv.port;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-function getContent(req, res){
-    fs.readFile('./public/index.html', 'utf-8', (err, data) => {
-        if (err) return console.error(err);
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data)
-        return res.end()
-    });
-}
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
+let content;
+fs.readFile('./public/index.html', 'utf-8', (err, data) => {
+    if (err) return console.error(err);
+    content = data;
+});
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -37,15 +34,13 @@ function getContent(req, res){
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) => {
-    getContent(req, res);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    return res.end(content);
 })
-
-
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port)
-console.log('Server listening on port', port)
-
+console.log('Server listening on port ${port}')
 
 // That's it! You're all done!
